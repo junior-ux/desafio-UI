b4bbadb6fce1f6ef8ecf424b5168e0bb9aeb5c28
junior-ux <55417250+junior-ux@users.noreply.github.com>
2021-07-25 17:37:47 -0300

    Importação google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +9,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter teste',
       theme: ThemeData.dark(),
-      home: MyHomePage(title: 'teste'),
+      home: MyHomePage(title: 'Teste fonte'),
     );
   }
 }
@@ -32,15 +32,17 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title), centerTitle: true,
+        title: Text(
+          widget.title,
+          style: GoogleFonts.chelaOne(),
+        ),
+        centerTitle: true,
         backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Column(
-          
-        ),
+        child: Column(),
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
